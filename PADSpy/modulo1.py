f5eedbb5c03bab6444ca9dcfc70824c3c70bc9a6
IsraelHuentecura <israel.huentecura@mayor.cl>
2024-06-19 01:30:59 -0400

    subo programa corriendo

--- PADSpy/modulo1.py
+++ PADSpy/modulo1.py
@@ -1,32 +1,59 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class MatrizDeAdyacencia:
-    def __init__(self, matriz):
-        # Verificamos que la matriz sea cuadrada
+    def __init__(self, matriz,vector):
         if matriz.shape[0] != matriz.shape[1]:
             raise ValueError("La matriz debe ser cuadrada (misma cantidad de filas y columnas)")
 
         self.matriz = matriz
+        self.vector_intervencion = vector
 
     def obtener_inversa_negativa(self):
-        # Calculamos la inversa negativa de la matriz
         inversa_negativa = -np.linalg.inv(self.matriz)
         return inversa_negativa
 
-    def agregar_vector_intervencion(self, vector):
-        # Verificamos que el vector tenga la misma cantidad de elementos que la matriz
-        if len(vector) != self.matriz.shape[0]:
+    def agregar_vector_intervencion(self):
+        if len(self.vector_intervencion) != self.matriz.shape[0]:
             raise ValueError("El vector de intervención debe tener la misma cantidad de elementos que la matriz")
 
-        # Agregamos el vector de intervención al final de las columnas
-        matriz_con_intervencion = np.hstack((self.matriz, vector.reshape(-1, 1)))
-
-        # Agregamos una fila de ceros al final para mantener la matriz cuadrada
+        matriz_con_intervencion = np.hstack((self.matriz, self.vector_intervencion.reshape(-1, 1)))
         matriz_con_fila_ceros = np.vstack((matriz_con_intervencion, np.zeros(matriz_con_intervencion.shape[1])))
-
-        # Agregamos el valor -1 al final del vector de intervención
         matriz_con_fila_ceros[-1, -1] = -1
 
         return matriz_con_fila_ceros
 
+    def probar_robustez(self, num_muestras=3):
+        """Probar la robustez de la matriz original variando la matriz de adyacencia segun una distribución normal y calculando la inversa negativa de cada una.
+        
+
+        Args:
+            num_muestras (int, optional): _description_. Defaults to 1000.
+            
+        """
+        resultados = []
+        matriz_original = self.matriz
+        for prueba in range(num_muestras):
+            variacion_matriz_adyacencia = np.random.normal(loc=0, scale=0.1, size=matriz_original.shape)
+            # Sumar la matriz original a la variación
+            variacion_matriz_adyacencia  = variacion_matriz_adyacencia + matriz_original
+         
+            # no en lo numerico si no en el formato
+            array_de_numeros = [numero for numero in variacion_matriz_adyacencia]
+            # Array a matriz
+            matriz_formato = np.array(array_de_numeros)
+            
+            matriz_con_intervencion = np.hstack((matriz_formato, self.vector_intervencion.reshape(-1, 1)))
+            matriz_con_fila_ceros = np.vstack((matriz_con_intervencion, np.zeros(matriz_con_intervencion.shape[1])))
+            matriz_con_fila_ceros[-1, -1] = -1 
+            
+            # Calcular la inversa negativa de la matriz con intervención
+            inversa_negativa_con_intervencion = -np.linalg.inv(matriz_con_fila_ceros)
+            
+            # Solo nos interesa la última columna de la matriz que corresponde a la intervención
+            resultados.append(inversa_negativa_con_intervencion[:-1, -1])
+            
+        
+        return resultados
+    
 
--- main.py
+++ main.py
@@ -1,25 +1,95 @@
 from PADSpy.modulo1 import MatrizDeAdyacencia
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Ejemplo de uso
-matriz_original = np.array([[12, 1, 2], [1, 3, 76], [0, 5, 4]])
-vector_intervencion = np.array([0.5, -0.2, 0.1])
-objeto_matriz = MatrizDeAdyacencia(matriz_original)
+# Leer la matriz de adyacencia
+matriz_original = pd.read_csv('matriz_adjacencia.csv')
+nombre_de_variables = matriz_original.columns
+nombre_de_variables = nombre_de_variables[1:]
+# Todos los nan los reemplazamos por 0
+matriz_original = matriz_original.fillna(0)
+# Borrar la primera columnas y la fila de los nombres
+matriz_original = matriz_original.drop(matriz_original.columns[0], axis=1)
+# Pasar a numpy
+matriz_original = matriz_original.to_numpy()
+# Agregar en la diagonal principal -1
+np.fill_diagonal(matriz_original, -1)
+print(matriz_original)
+
+n_variables = matriz_original.shape[0]
+# Vector de intervención de -1 a 1 en este caso son 14
+vector_intervencion = [0.2, 0.3, 0.4, 0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.3]
+vector_intervencion = np.array(vector_intervencion)
+objeto_matriz = MatrizDeAdyacencia(matriz_original, vector_intervencion)
 
 # Agregamos el vector de intervención y la fila de ceros
-matriz_con_intervencion = objeto_matriz.agregar_vector_intervencion(vector_intervencion)
+matriz_con_intervencion = objeto_matriz.agregar_vector_intervencion()
 print("Matriz con vector de intervención y fila de ceros:")
 print(matriz_con_intervencion)
 
 # Calculamos la inversa negativa de la matriz original y de la matriz con intervención
 inversa_negativa_original = objeto_matriz.obtener_inversa_negativa()
-inversa_negativa_con_intervencion = MatrizDeAdyacencia(matriz_con_intervencion).obtener_inversa_negativa()
+inversa_negativa_con_intervencion = MatrizDeAdyacencia(matriz_con_intervencion[:-1, :-1], vector_intervencion).obtener_inversa_negativa()
 print("\nInversa negativa de la matriz original:")
 print(inversa_negativa_original)
 print("\nInversa negativa de la matriz con intervención:")
 print(inversa_negativa_con_intervencion)
 
-# Graficar la ultima columna de la matriz con intervencion como grafico de barras
-import matplotlib.pyplot as plt
-plt.bar(range(len(vector_intervencion)), vector_intervencion)
+# Extraer la última columna de la matriz con intervención
+ultima_columna = inversa_negativa_con_intervencion[:, -1]
+
+
+# Tamaño de la figura
+plt.figure(figsize=(15, 9))
+
+# Graficar la última columna de la matriz con intervención como gráfico de barras
+
+plt.bar(nombre_de_variables, ultima_columna)
+plt.title('Intervención sobre las variables')
+# Rotar los nombres de las variables para que no se superpongan
+plt.xticks(rotation=45)
+# Cuidar que se vean los nombres de las variables enteros y no se corten
+plt.tight_layout()
+plt.xlabel('Variables')
+plt.ylabel('Valores de intervención')
+plt.show()
+
+# Probar la robustez de la matriz original
+vectores_intervenciones_simuladas = objeto_matriz.probar_robustez(num_muestras=3)
+# Graficar el promedio de las intervenciones simuladas versus la intervención original para ver la robustez del modelo
+# Poniendo las barras una al lado de otra para comparar
+promedio_intervenciones_simuladas = np.mean(vectores_intervenciones_simuladas, axis=0)
+
+# Ancho de cada barra
+bar_width = 0.35
+plt.figure(figsize=(16, 9))
+
+# Posiciones de las barras
+r1 = np.arange(len(vector_intervencion))
+r2 = [x + bar_width for x in r1]
+
+plt.bar(r1, vector_intervencion, width=bar_width, tick_label=nombre_de_variables, label='Intervención original')
+plt.bar(r2, promedio_intervenciones_simuladas, width=bar_width, label='Promedio intervenciones simuladas')
+
+
+# Agregar lineas horizontales que permitan discriminar si la intervención es significativa o no
+# Esto se basa en el 10% de el valor mas grande y el 10% del valor mas pequeño
+maximo_valor = max(vector_intervencion)
+minimo_valor = min(vector_intervencion)
+# 10% del valor mas grande
+linea_superior = 0.1*maximo_valor
+# 10% del valor mas pequeño
+linea_inferior = -0.1*minimo_valor
+
+plt.axhline(y=linea_superior, color='r', linestyle='--', label='10% del valor más grande')
+plt.axhline(y=linea_inferior, color='g', linestyle='--', label='10% del valor más pequeño')
+# Rotar los nombres de las variables para que no se superpongan
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.title('Robustez del modelo')
+plt.xlabel('Variables')
+plt.ylabel('Valores de intervención')
+plt.legend()
+# Abrir la ventana de visualización en un tamaño adecuado
 plt.show()
